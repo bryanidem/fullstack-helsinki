5b8649ca34491b12043d530f525c17a71923782c
Bryan Medina <bryan.medina.per@gmail.com>
2023-07-18 08:14:26 -0600

    add error handling for person creation

--- part3/index.js
+++ part3/index.js
@@ -56,9 +56,15 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  if (!body.name) {
+  if (!body.name && !body.number) {
     return response.status(400).json({ error: "content missing" });
   }
+
+  const nameExists = persons.some((person) => person.name === body.name);
+  if (nameExists) {
+    return response.status(400).json({ error: "name must be unique" });
+  }
+
   const person = {
     id: Math.floor(Math.random() * 1000),
     name: body.name,
