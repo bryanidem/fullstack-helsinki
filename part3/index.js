d93d8adda049e5f8e86ee84bfb34c02fa726fe13
Bryan Medina <bryan.medina.per@gmail.com>
2023-08-06 10:44:37 -0600

    add delete person to mongoDB

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require("./models/person");
+const { default: mongoose } = require("mongoose");
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -15,31 +18,33 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -68,28 +73,41 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  if (!body.name && !body.number) {
-    return response.status(400).json({ error: "content missing" });
-  }
-
-  const nameExists = persons.some((person) => person.name === body.name);
-  if (nameExists) {
-    return response.status(400).json({ error: "name must be unique" });
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 1000),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person.save().then((result) => {
+    // mongoose.connection.close();
+    response.json(result);
+  });
 });
+
+// app.post("/api/persons", (request, response) => {
+//   const body = request.body;
+//   if (!body.name && !body.number) {
+//     return response.status(400).json({ error: "content missing" });
+//   }
+
+//   const nameExists = persons.some((person) => person.name === body.name);
+//   if (nameExists) {
+//     return response.status(400).json({ error: "name must be unique" });
+//   }
+
+//   const person = {
+//     id: Math.floor(Math.random() * 1000),
+//     name: body.name,
+//     number: body.number,
+//   };
+
+//   persons = persons.concat(person);
+//   response.json(person);
+// });
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
